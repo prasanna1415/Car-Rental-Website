53bbb395488665a1e6179798e5f62eb18e219158
Bishal Adhikari <Vishaladhikari911@gmail.com>
2024-05-23 23:57:51 +0545

    mode project more attractive

--- Client/main.js
+++ Client/main.js
@@ -1,21 +1,21 @@
-const sr= ScrollReveal(
-    {
-        distance : '60px',
-        duration : 2500,
-        delay: 400,
-        reset:true,
+// const sr= ScrollReveal(
+//     {
+//         distance : '60px',
+//         duration : 2500,
+//         delay: 400,
+//         reset:true,
 
-    }
-)
+//     }
+// )
 
-sr.reveal('.text',{delay:200,origin:'top'})
-sr.reveal('.form-container form',{delay:800,origin:'left'})
-sr.reveal('.heading',{delay:600,origin:'top'})
-sr.reveal('.ride-container .box',{delay:400,origin:'top'})
-sr.reveal('.services-container .box',{delay:400,origin:'top'})
-sr.reveal('.about-container .box',{delay:400,origin:'top'})
-sr.reveal('.reviews-container',{delay:400,origin:'top'})
-sr.reveal('.newsletter .box',{delay:400,origin:'bottom'})
+// sr.reveal('.text',{delay:200,origin:'top'})
+// sr.reveal('.form-container form',{delay:800,origin:'left'})
+// sr.reveal('.heading',{delay:600,origin:'top'})
+// sr.reveal('.ride-container .box',{delay:400,origin:'top'})
+// sr.reveal('.services-container .box',{delay:400,origin:'top'})
+// sr.reveal('.about-container .box',{delay:400,origin:'top'})
+// sr.reveal('.reviews-container',{delay:400,origin:'top'})
+// sr.reveal('.newsletter .box',{delay:400,origin:'bottom'})
 
 
 
@@ -24,20 +24,24 @@ sr.reveal('.newsletter .box',{delay:400,origin:'bottom'})
 //     document.getElementById('signin-popup').style.display = 'flex';
 // }
      
+// Open Sign-up Popup
+// function openSignupPopup() {
+//     document.getElementById('signup-popup').style.display = 'flex';
+// }
 
 // Close Popup
-function closePopup(popupId) {
-    document.getElementById(popupId).style.display = 'none';
-}
+// function closePopup(popupId) {
+//     document.getElementById(popupId).style.display = 'none';
+// }
 
-// Close popup when clicking outside
-document.addEventListener('click', function(event) {
-    var popups = document.querySelectorAll('.popup-container');
-    for (var i = 0; i < popups.length; i++) {
-        var popup = popups[i];
-        if (event.target === popup) {
-            popup.style.display = 'none';
-        }
-    }
-});
+// // Close popup when clicking outside
+// document.addEventListener('click', function(event) {
+//     var popups = document.querySelectorAll('.popup-container');
+//     for (var i = 0; i < popups.length; i++) {
+//         var popup = popups[i];
+//         if (event.target === popup) {
+//             popup.style.display = 'none';
+//         }
+//     }
+// });
 
--- Server/routes/signUp.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.route("/").get((req,res)=>{
-    res.status(200).send("Hello this is signup get");
-});
-
-module.exports = router;
