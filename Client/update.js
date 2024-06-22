32c65f43cea0d4af705a26655a8c40dc1437efb2
Bishal Adhikari <Vishaladhikari911@gmail.com>
2024-06-22 22:45:15 +0545

    set-up for post update page

--- Client/update.js
+++ Client/update.js
@@ -1,1 +1,47 @@
-document.getElementById('car-photo').value = 'xyx.com';
+
+//update options
+
+const photo = document.querySelector('.photo');
+const price = document.querySelector('.price');
+const carName = document.querySelector('.modelname');
+
+//change div
+
+const picture = document.querySelector('.picture');
+const carPrice = document.querySelector('.money');
+const carModel = document.querySelector('.carname');
+
+//main div
+
+const mainDiv = document.querySelector('.main');
+
+//close button
+const closeBtn = document.querySelectorAll('.close-btn');
+
+
+
+
+photo.addEventListener('click',()=> {
+    picture.style.zIndex = 1;
+    mainDiv.style.zIndex = 0;
+} )
+
+price.addEventListener('click',()=> {
+    carPrice.style.zIndex = 1;
+    mainDiv.style.zIndex = 0;
+} )
+
+carName.addEventListener('click',()=> {
+    carModel.style.zIndex = 1;
+    mainDiv.style.zIndex = 0;
+} )
+
+
+closeBtn.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        mainDiv.style.zIndex = 1;
+        picture.style.zIndex = 0;
+        carPrice.style.zIndex = 0;
+        carModel.style.zIndex = 0;
+    })
+})
