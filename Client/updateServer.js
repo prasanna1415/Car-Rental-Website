bef898ff1901f8f62e0e4617ad63746f048f7ac6
Bishal Adhikari <Vishaladhikari911@gmail.com>
2024-06-23 19:55:44 +0545

    update page ui finishing touch

---
+++ Client/updateServer.js
@@ -0,0 +1,31 @@
+const picture = document.querySelector('.picture');
+const carPrice = document.querySelector('.money');
+const carModel = document.querySelector('.carname');
+
+//submit btn for each element
+const photoBtn = document.querySelector('.photo-submit');
+const priceBtn = document.querySelector('.number-submit');
+const carNameBtn = document.querySelector('.modelname-submit');
+
+
+
+photoBtn.addEventListener('click',()=>{
+    if(picture){
+
+    }
+})
+
+
+priceBtn.addEventListener('click',()=>{
+    if(carPrice){
+
+    }
+})
+
+
+carNameBtn.addEventListener('click',()=>{
+    if(carModel){
+        
+    }
+})
+
