8f39450511aada0df536721e21ab4ec62153967f
Bishal adhikari <vishaladhikari911@gmail.com>
2024-06-25 13:16:37 +0545

    sending info to backend for update post:

--- Client/updateServer.js
+++ Client/updateServer.js
@@ -1,6 +1,6 @@
-const picture = document.querySelector('.picture');
-const carPrice = document.querySelector('.money');
-const carModel = document.querySelector('.carname');
+const pictureInput = document.querySelector('.updated-photo');
+const carPriceInput= document.querySelector('.updated-price');
+const carModelInput = document.querySelector('.updated-name');
 
 //submit btn for each element
 const photoBtn = document.querySelector('.photo-submit');
@@ -9,23 +9,51 @@ const carNameBtn = document.querySelector('.modelname-submit');
 
 
 
-photoBtn.addEventListener('click',()=>{
-    if(picture){
+photoBtn.addEventListener('click',async()=>{
 
+    const carPhoto = pictureInput.value;
+    
+    
+
+
+    if(carPhoto){
+        
+        try{
+            const response = await fetch(' backend link here',{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ imageUrl: carPhoto })
+            })
+        }catch(e){
+            console.log(e);
+        }
     }
 })
 
 
 priceBtn.addEventListener('click',()=>{
+    const carPrice = carPriceInput.value;
     if(carPrice){
+        try{
+
+        }catch(e){
+
+        }
 
     }
 })
 
 
 carNameBtn.addEventListener('click',()=>{
-    if(carModel){
-        
+    const carName = carModelInput.value;
+    if(carName){
+        try{
+
+        }catch(e){
+
+        }
     }
 })
 
